a8c74d09094b408b20c313152282a853a932963f
spiderforrest <spider@spood.org>
2023-03-10 12:12:13 -0800

    okay pretty sure this fixes vim-colemak

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -18,7 +18,7 @@ require("lazy").setup(
         { 'spiderforrest/vim-colemak', event = 'BufEnter', -- running it before everything else bc i'll start typing lol
         -- flag off if Lily isn't here :(
             enabled=function()
-                if fn.system({"grep", "-c", "Lily58"}, fn.system('lsusb')) then
+                if fn.system({"grep", "-c", "Lily58"}, fn.system('lsusb')) > 0 then -- dirty whatever
                     return true
                 end
                 return false
