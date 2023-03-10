ac5b5558eafaf49b2fe6464c555b43e0a8c9504e
spiderforrest <spider@spood.org>
2023-03-10 12:14:13 -0800

    aack okay THIS time i've only been spamming commits to pull on my server to test lol

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -18,7 +18,7 @@ require("lazy").setup(
         { 'spiderforrest/vim-colemak', event = 'BufEnter', -- running it before everything else bc i'll start typing lol
         -- flag off if Lily isn't here :(
             enabled=function()
-                if fn.system({"grep", "-c", "Lily58"}, fn.system('lsusb')) > 0 then -- dirty whatever
+                if fn.system({"grep", "-c", "Lily58"}, fn.system('lsusb')) + 0 > 0 then -- dirty whatever
                     return true
                 end
                 return false
