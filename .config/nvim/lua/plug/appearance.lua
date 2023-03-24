ad9fe2eece9ed519a25b3a33decf7dca27244861
spiderforrest <spider@spood.org>
2023-03-24 14:50:35 -0700

    removed duplicate code

--- .config/nvim/lua/plug/appearance.lua
+++ .config/nvim/lua/plug/appearance.lua
@@ -61,57 +61,6 @@ return {
     { 'junegunn/goyo.vim', cmd = 'Goyo' }, -- make that lil window in the middle that i like
     { 'junegunn/limelight.vim', cmd = 'Limelight' }, -- highlight current block brighter
     -- ui revamp
-    -- {{{ bar
-    { 'nvim-lualine/lualine.nvim', event = 'VeryLazy', config = function()
-        require('lualine').setup {
-            options = {
-                theme = 'everforest',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
-                disabled_filetypes = { statusline = { 'chadtree', 'CHADTree', 'undotree_2', 'diffpanel_3' } },
-            },
-            sections = {
-                lualine_a = {line_sym_1, 'mode', 'searchcount'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = { {
-                    'filename',
-                    symbols = {
-                        modified = '+',
-                        readonly = '-',
-                        unnamed = '/',
-                        newfile = '!'
-                    },
-                    -- if null, hide
-                    cond = function()
-                        if fn.expand('%:t') == '' then
-                            return false
-                        end
-                        return true
-                    end
-                }},
-                lualine_x = {'fileformat', line_wc},
-                lualine_y = { 'encoding', { 'filetype', icons_enabled = false } },
-                lualine_z = { 'progress', 'location', line_sym_2}
-            },
-            inactive_sections = {
-                lualine_c = {
-                    {'filename',
-                    -- if null, hide
-                    cond = function()
-                        if fn.expand('%:t') == '' then
-                            return false
-                        end
-                        return true
-                    end },
-                    'filetype'},
-                lualine_x = {'location'},
-            },
-            tabline = {},
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
-        }
-    end }, -- }}}
     { 'kyazdani42/nvim-web-devicons', event = 'VeryLazy' }, -- icons and emojis n shit: 🗿
     { 'eandrju/cellular-automaton.nvim', event = 'VeryLazy' }, -- no comment.
 }
