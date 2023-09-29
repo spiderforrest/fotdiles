622b47dd4d300559b690f5370129a38fc8932ca3
spiderforrest <spider@spood.org>
2023-09-28 21:57:20 -0700

    fixed the clicky tags

--- .config/awesome/gui.lua
+++ .config/awesome/gui.lua
@@ -6,36 +6,34 @@ local naughty = require("naughty")
 
 -- {{{ def functions
 -- list of clients
-function clientmenu(filter, selected_tags_only)
-  local scr, items, clients = awful.screen.focused(), {
-    theme = {
+function clientmenu()
+  local clients = client.get()
+
+  -- create the options from the list of clients
+  local items = {}
+  for _, c in ipairs(clients) do
+    table.insert(items, {
+      c.name,
+      function() c:jump_to() end,
+      c.icon
+    })
+  end
+
+  -- pretty it
+  table.sort(items, function(a, b) return a[1] < b[1] end) -- alphabetize
+  items.theme = {
       width        = beautiful.clientsmenu_width,
       border_color = beautiful.clientsmenu_border_color
     }
+
+  -- rendering
+  local menu, geom = awful.menu(items), awful.screen.focused().geometry
+  menu:show{
+    coords = {
+      x = geom.x + (geom.width - menu.width) / 2,
+      y = geom.y + (geom.height - menu.height) / 2
+    }
   }
-
-  if selected_tags_only then
-    clients = gears.table.join(unpack(gears.table.map(function(t) return t:clients() end, scr.selected_tags)))
-  else
-    clients = client.get()
-  end
-
-  for c in gears.table.iterate(clients, function(c) return awful.rules.match(c, filter) end) do
-  table.insert(items, {
-    c.name,
-    function() c:jump_to() end,
-    c.icon
-  })
-end
-
-table.sort(items, function(a, b) return a[1] < b[1] end)
-local menu, geom = awful.menu(items), scr.geometry
-menu:show{
-  coords = {
-    x = geom.x + (geom.width - menu.width) / 2,
-    y = geom.y + (geom.height - menu.height) / 2
-  }
-}
 end
 
 local function set_wallpaper(s)
@@ -56,29 +54,29 @@ local function set_wallpaper(s)
 end
 
 local scroll_tag_buttons = gears.table.join(
-awful.button({ }, 5, function(t)
-  local s = awful.screen.focused()
-  if not s.selected_tag then return end
-  if #s.tags == s.selected_tag.index then return end --prevent scrolling wrap
-  awful.tag.viewnext(t.screen)
-end),
-awful.button({ }, 4, function(t)
-  local s = awful.screen.focused()
-  if not s.selected_tag then return end
-  if 1 == s.selected_tag.index then return end --prevent scrolling wrap
-  awful.tag.viewprev(t.screen)
-end)
+  awful.button({ }, 5, function(t)
+    local s = awful.screen.focused()
+    if not s.selected_tag then return end
+    if #s.tags == s.selected_tag.index then return end --prevent scrolling wrap
+    awful.tag.viewnext(t.screen)
+  end),
+  awful.button({ }, 4, function(t)
+    local s = awful.screen.focused()
+    if not s.selected_tag then return end
+    if 1 == s.selected_tag.index then return end --prevent scrolling wrap
+    awful.tag.viewprev(t.screen)
+  end)
 )
 
 local taglist_buttons = gears.table.join(
-  awful.button({ }, 1, function(t) naughty.notify{text=tostring(t)} end),
+  awful.button({ }, 1, function(t) t:view_only() end),
   awful.button({ shft }, 1, function(t)
     if client.focus then
       client.focus:move_to_tag(t)
     end
   end),
   awful.button({ }, 3, awful.tag.viewtoggle),
-  awful.button({ modkey }, 3, function(t)
+  awful.button({ meta }, 3, function(t)
     if client.focus then
       client.focus:toggle_tag(t)
     end
@@ -87,10 +85,10 @@ local taglist_buttons = gears.table.join(
 )
 
 local layoutbox_buttons = gears.table.join(
-awful.button({ }, 1, function () awful.layout.inc( 1) end),
-awful.button({ }, 3, function () awful.layout.inc(-1) end),
-awful.button({ }, 4, function () awful.layout.inc( 1) end),
-awful.button({ }, 5, function () awful.layout.inc(-1) end)
+  awful.button({ }, 1, function () awful.layout.inc( 1) end),
+  awful.button({ }, 3, function () awful.layout.inc(-1) end),
+  awful.button({ }, 4, function () awful.layout.inc( 1) end),
+  awful.button({ }, 5, function () awful.layout.inc(-1) end)
 )
 
 local no_widget_tagscrolling = gears.table.join(
@@ -123,13 +121,15 @@ awful.screen.connect_for_each_screen(function(s)
 
   -- the layout icon/scroller
   s.layout_box = awful.widget.layoutbox(s)
+  s.layout_box:buttons(layoutbox_buttons)
   -- create a taglist widget
   s.taglist = awful.widget.taglist{
     screen  = s,
     -- hide tags greater than 5 if empty and nonfocused
     filter  = function (t) return (t.index < 6 or #t:clients() > 0 or t.selected) end,
+    buttons = taglist_buttons -- if you assign with taglist:buttons() it sends different data??? ok
   }
-  s.taglist:buttons(taglist_buttons)
+  -- s.taglist:buttons(taglist_buttons)
   -- global titlebar title container
   s.title_container = wibox.container.margin()
   s.title_container:buttons(scroll_tag_buttons)
@@ -167,55 +167,44 @@ awful.screen.connect_for_each_screen(function(s)
 end) -- }}}
 
 -- i'm soooo fancy
-require("py32awe").setup{container = statusline_container, bar_command = "script -qfec 'py3status' -O '/dev/null'"
-  -- module_override_handler = function (module, widget)
-  --   -- the clock makes the whole thing wiggle wiggle wiggle, stop that
-  --   if module.name == "tztime" then
-  --     widget.forced_width = 162
-  --   elseif module.name == 'async_script' then
-  --     widget.forced_width = 168
-  --   elseif module.name == 'clock' then -- the est one
-  --     widget.forced_width = 105
-  --   end
-  -- end
-}
+require("py32awe").setup{container = statusline_container, bar_command = "script -qfec 'py3status' -O '/dev/null'" }
 
-    -- {{{ global titlebar
-    local function title_create(c)
-      return wibox.widget {
-        markup = "<i>" .. gears.string.xml_escape(c.name or "/someone should have set this sooner/") .. "</i>",
-        align = "center",
-        widget = wibox.widget.textbox,
-      }
+-- {{{ global titlebar
+local function title_create(c)
+  return wibox.widget {
+    markup = "<i>" .. gears.string.xml_escape(c.name or "/someone should have set this sooner/") .. "</i>",
+    align = "center",
+    widget = wibox.widget.textbox,
+  }
+end
+
+local function title_insert(c)
+  if not c.title then
+    c.title = title_create(c)
+  end
+  c.screen.title_container.widget = c.title
+  c.title_container = c.screen.title_container
+end
+
+local function title_update(c)
+  if c.title then
+    c.title:set_markup("<i>" .. gears.string.xml_escape(c.name or "/uh oh, not found/") .. "</i>")
+  end
+end
+
+local function title_remove(c)
+  -- delay unsetting of titlebar text to remove flickering on change
+  gears.timer.delayed_call(function(title, container)
+    if title and container and container.widget == title then
+      container.widget = nil
     end
+  end, c.title, c.title_container)
+end
 
-    local function title_insert(c)
-      if not c.title then
-        c.title = title_create(c)
-      end
-      c.screen.title_container.widget = c.title
-      c.title_container = c.screen.title_container
-    end
+client.connect_signal("property::name", title_update)
+client.connect_signal("focus", title_insert)
+client.connect_signal("unfocus", title_remove)
 
-    local function title_update(c)
-      if c.title then
-        c.title:set_markup("<i>" .. gears.string.xml_escape(c.name or "/uh oh, not found/") .. "</i>")
-      end
-    end
+-- }}}
 
-    local function title_remove(c)
-      -- delay unsetting of titlebar text to remove flickering on change
-      gears.timer.delayed_call(function(title, container)
-        if title and container and container.widget == title then
-          container.widget = nil
-        end
-      end, c.title, c.title_container)
-    end
-
-    client.connect_signal("property::name", title_update)
-    client.connect_signal("focus", title_insert)
-    client.connect_signal("unfocus", title_remove)
-
-    -- }}}
-
-    -- vim: foldmethod=marker
+-- vim: foldmethod=marker
